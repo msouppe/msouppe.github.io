c149f250901a3c033c29f2151a6716f879d84016
msouppe <msouppe@gmail.com>
2016-11-15 22:48:01 -0800

    slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
-var a = $(".nav").offset().top;
+var slideIndex = 1;
+showSlides(slideIndex);
 
-$(document).scroll(function(){
-    if($(this).scrollTop() > a)
-    {   
-       $('.nav').css({"background":"red"});
-    } else {
-       $('.nav').css({"background":"transparent"});
-    }
-});
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
